914661214286bd3657bb70b4d05e02dbb80b74d9
MD <43723938+MaelMD@users.noreply.github.com>
2023-05-22 13:08:22 +0100

    Add files via upload

---
+++ Hash.py
@@ -0,0 +1,24 @@
+from passlib.hash import pbkdf2_sha256
+from base64 import b64decode
+from passlib.utils.binary import ab64_encode
+
+def hashAndCompare(crackedHash):
+    
+    crackedChain = crackedHash.split('$')   
+    #crackedChainDigest = crackedChain[0]
+    crackedChainRounds = crackedChain[1]
+    crackedChainSalt = crackedChain[2]
+    crackedChainSaltPasslibFormat = ab64_encode(crackedChainSalt.encode('utf8')).decode('utf8')
+    crackedChainHashData = crackedChain[3].split(':')
+    crackedChainHash = crackedChainHashData[0]
+    crackedChainHashPasslibFormat = ab64_encode(b64decode(crackedChainHash)).decode('utf8')
+    crackedChainData = crackedChainHashData[1]
+    
+    passlibHash = pbkdf2_sha256.hash(crackedChainData, rounds=crackedChainRounds, salt=crackedChainSalt.encode('utf8')) 
+    passlibChain = passlibHash.split('$')
+    passlibChainSalt = passlibChain[3]
+    passlibChainHash = passlibChain[4]
+    
+    print('Passlib: Hash: {0} Salt: {1}\nCracked: Hash: {2} Salt: {3}\n'.format(passlibChainHash, passlibChainSalt, crackedChainHashPasslibFormat, crackedChainSaltPasslibFormat))
+
+hashAndCompare('pbkdf2_sha256$24000$D3KTzHOCeZbe$JJJxUks6FW+JhvG1quwZau9dFy9eVhOJIZbUYdbJ+as=:n592001n')
